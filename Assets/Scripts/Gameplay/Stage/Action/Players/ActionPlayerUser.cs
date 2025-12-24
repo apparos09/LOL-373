9999bf52df98921b67c6e81ba68702970dd7d672
mecha-rm <roderick.montague243@gmail.com>
2025-12-24 18:07:53 -0500

    Working on Unit Select Buttons for Player User
    Working on the unit select buttons that the player will use in the action stages.

--- Assets/Scripts/Gameplay/Stage/Action/Players/ActionPlayerUser.cs
+++ Assets/Scripts/Gameplay/Stage/Action/Players/ActionPlayerUser.cs
@@ -7,6 +7,9 @@ namespace RM_EDU
     // The user action player.
     public class ActionPlayerUser : ActionPlayer
     {
+        // The unit prefab the action player user has selected.
+        private ActionUnit selectedUnitPrefab;
+
         // Start is called before the first frame update
         protected override void Start()
         {
@@ -17,6 +20,30 @@ namespace RM_EDU
             {
                 actionManager.playerUser = this;
             }
+        }
+
+        // Returns 'true' if the player is selecting a unit.
+        public bool IsSelectingUnitPrefab()
+        {
+            return selectedUnitPrefab != null;
+        }
+
+        // Gets the unit the player has selected.
+        public ActionUnit GetSelectedUnitPrefab()
+        {
+            return selectedUnitPrefab;
+        }
+
+        // Sets the prefab the player has selected. This should be a prefab, not an actual object.
+        public void SetSelectedUnitPrefab(ActionUnit unitPrefab)
+        {
+            selectedUnitPrefab = unitPrefab;
+        }
+
+        // Clears the prefab the player has selected.
+        public void ClearSelectedUnitPrefab()
+        {
+            selectedUnitPrefab = null;
         }
 
         // Update is called once per frame
--- Assets/Scripts/Gameplay/Stage/Action/Units/Buttons/ActionUnitButton.cs
+++ Assets/Scripts/Gameplay/Stage/Action/Units/Buttons/ActionUnitButton.cs
@@ -2,6 +2,7 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
+using TMPro;
 
 namespace RM_EDU
 {
@@ -14,8 +15,25 @@ namespace RM_EDU
         // The button script.
         public Button button;
 
+        // The name of the unit.
+        public TMP_Text unitNameText;
+
+        // The energy cost text. This displays the amount of energy needed to create the unit.
+        public TMP_Text energyCostText;
+
+        // The highlight on the top of the card, which indicates the card above is usable.
+        [Tooltip("An image above the card that indicates the card above the current one is usable.")]
+        public Image topHighlightImage;
+
+        // The highlight on the bottom of the card, which indicates the card below is usable.
+        [Tooltip("An image below the card that indicates the card below the current one is usable.")]
+        public Image bottomHighlightImage;
+
+        // The unit prefab.
+        public ActionUnit unitPrefab;
+
         // Start is called before the first frame update
-        void Start()
+        protected virtual void Start()
         {
             // Gets the instance.
             if (actionUI == null)
@@ -24,12 +42,53 @@ namespace RM_EDU
             // If the button isn't set, try to get the component in the children.
             if (button == null)
                 button = GetComponentInChildren<Button>();
+
+
+            // Adds a Select() call on the button.
+            if (button != null)
+            {
+                // Listener for the tutorial toggle.
+                button.onClick.AddListener(delegate
+                {
+                    Select();
+                });
+            }
         }
 
-        // Update is called once per frame
-        void Update()
+        // Called when the button has been pressed.
+        public virtual void Select()
         {
+            // Gets the player user.
+            ActionPlayerUser playerUser = actionUI.actionManager.playerUser;
 
+            // If the player exists, give it this unit prefab.
+            if(playerUser != null)
+            {
+                // Give the player the selected prefab.
+                playerUser.SetSelectedUnitPrefab(unitPrefab);
+            }
         }
+
+        // Gets the unit type.
+        public virtual ActionUnit.unitType GetUnitType()
+        {
+            // The unit type.
+            ActionUnit.unitType unitType;
+
+            // Prefab exists.
+            if(unitPrefab != null)
+            {
+                unitType = unitPrefab.GetUnitType();
+            }
+            else
+            {
+                // There's no prefab, so return unknown type.
+                unitType = ActionUnit.unitType.unknown;
+            }
+
+            // Returns the unit type.
+            return unitType;
+        }
+
     }
 }
