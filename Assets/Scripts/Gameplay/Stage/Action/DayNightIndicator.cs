05d804a06700d112f85c5845938ac53ddf5c4729
mecha-rm <roderick.montague243@gmail.com>
2026-01-17 00:09:14 -0500

    Added Day-Night Indicator Images
    The day-night indicator images have been updated. The colours used don't exactly match up with the day-night colour transition, but the display image itself is alright. May adjust the transition period for going from day to night visually.

--- Assets/Scripts/Gameplay/Stage/Action/DayNightIndicator.cs
+++ Assets/Scripts/Gameplay/Stage/Action/DayNightIndicator.cs
@@ -12,17 +12,32 @@ namespace RM_EDU
         // The action ui.
         public ActionUI actionUI;
 
-        // The image being used.
-        // TODO: update with a proper animation. Right now, the image colour changing is the only indicator.
-        public Image image;
+        // The display image.
+        public Image displayImage;
+
+        // The day-to-night sprite.
+        public Sprite dayToNightSprite;
+
+        // The night-to-day sprite.
+        public Sprite nightToDaySprite;
+
+        [Header("Tracker")]
+
+        // The tracker used to show the time.
+        public Image trackerImage;
+
+        // The start position of the tracker.
+        public GameObject trackerStartPos;
+
+        // The end position of the tracker.
+        public GameObject trackerEndPos;
+
+        [Header("Update")]
 
         // NOTE: the day and night colors will likely be taken out once a proper graphic is created.
-
-        // The day color.
-        private Color dayColor = Color.yellow;
-
-        // The night color.
-        private Color nightColor = Color.blue;
+        // The day and night colors.
+        // private Color dayColor = Color.yellow;
+        // private Color nightColor = Color.blue;
 
         // If the wind indicator should be updated automatically.
         public bool autoUpdateIndicator = true;
@@ -36,6 +51,52 @@ namespace RM_EDU
             // Gets the instance.
             if (actionUI == null)
                 actionUI = ActionUI.Instance;
+
+            // NOTE: since the game doesn't take advantage of night-to-day...
+            // The game doesn't automatically change the display.
+            // In other words, the day-to-night display is the one that's always used.
+        }
+
+        // Automatically sets the display image based on if the stage is going from day to night or night to day.
+        public void AutoSetDisplayImage()
+        {
+            // Gets the instance.
+            ActionManager actionManager = ActionManager.Instance;
+
+            // Checks if going from night to day.
+            // Day to night is the default image.
+            if(actionManager.IsNightToDay())
+            {
+                SetDisplayToNightToDay();
+            }
+            else
+            {
+                SetDisplayToDayToNight();
+            }
+        }
+
+        // Returns 'true' if transitioning from day to night, which is checked using the sprite.
+        public bool IsDisplayDayToNight()
+        {
+            return displayImage.sprite == dayToNightSprite;
+        }
+
+        // Sets the display to go from day to night.
+        public void SetDisplayToDayToNight()
+        {
+            displayImage.sprite = dayToNightSprite;
+        }
+
+        // Returns 'true' if transition from night to day, which is checked using the sprite.
+        public bool IsDisplayNightToDay()
+        {
+            return displayImage.sprite == nightToDaySprite;
+        }
+
+        // Sets the display to go from night to day.
+        public void SetDisplayToNightToDay()
+        {
+            displayImage.sprite = nightToDaySprite;
         }
 
         // Updates the indicator.
@@ -50,8 +111,14 @@ namespace RM_EDU
                 // The t-value for going from day to night.
                 float t = actionManager.GetDayNightTimerProgress();
 
+                // Gets the tracker position using lerp.
+                Vector3 trackerPos = Vector3.Lerp(trackerStartPos.transform.position, trackerEndPos.transform.position, t);
+
+                // Sets the tracker's position.
+                trackerImage.transform.position = trackerPos;
+
                 // Lerps between the two colours.
-                image.color = Color.Lerp(dayColor, nightColor, t);
+                // displayImage.color = Color.Lerp(dayColor, nightColor, t);
             }
         }
 
