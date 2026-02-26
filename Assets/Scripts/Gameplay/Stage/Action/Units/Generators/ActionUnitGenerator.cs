db09a578d77eaf4935d3cd5c6cbec2e858c6f721
mecha-rm <roderick.montague243@gmail.com>
2026-02-25 21:30:03 -0500

    Adjusted the Stats of Various Generators
    Various generator units have had their stats adjusted for balancing.

--- Assets/Scripts/Gameplay/Stage/Action/Units/Generators/ActionUnitGenerator.cs
+++ Assets/Scripts/Gameplay/Stage/Action/Units/Generators/ActionUnitGenerator.cs
@@ -193,8 +193,8 @@ namespace RM_EDU
                 }
 
                 // Calculates the value based on the current energy generation speed.
-                // Calculation: 1 sec + 10 sec * ((maxSpeed - speed) / maxSpeed)
-                value = 1.0F + 10.0F * (BASE_STAT_MAXIMUM - Mathf.Abs(currEnergyGenSpeed)) / BASE_STAT_MAXIMUM;
+                // Calculation: 2 sec + 10 sec * ((maxSpeed - Abs(speed)) / maxSpeed)
+                value = 1.0F + 10.0F * ((BASE_STAT_MAXIMUM - Mathf.Abs(currEnergyGenSpeed)) / BASE_STAT_MAXIMUM);
 
                 // Bounds check.
                 if (value < 0)
