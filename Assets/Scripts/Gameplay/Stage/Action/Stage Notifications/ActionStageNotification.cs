7afe424c78096b14ecc4818fb7c5afadca1008db
mecha-rm <roderick.montague243@gmail.com>
2026-05-04 22:36:36 -0400

    Working on Enemy Approaching Notification
    Working on a script for showing a notification that enemies are about to start being spawned in an action stage.

---
+++ Assets/Scripts/Gameplay/Stage/Action/Stage Notifications/ActionStageNotification.cs
@@ -0,0 +1,81 @@
+using System.Collections;
+using System.Collections.Generic;
+using TMPro;
+using UnityEngine;
+using UnityEngine.UI;
+
+namespace RM_EDU
+{
+    // A notification that shows up in action stages.
+    public class ActionStageNotification : MonoBehaviour
+    {
+        // The action UI.
+        public ActionUI actionUI;
+
+        // The background image of the notification.
+        public Image backgroundImage;
+
+        // The notification text.
+        public TMP_Text text;
+
+        // The TMP text translator.
+        public TMP_TextTranslator textTranslator;
+
+        [Header("Animation")]
+        // The animator.
+        public Animator animator;
+
+        // The empty state animation.
+        public string emptyStateAnim = "Empty State";
+
+        // The blink animation.
+        public string blinkAnim = "Action Stage Notification - Blink Animation";
+
+        // Start is called before the first frame update
+        void Start()
+        {
+            // Gets the action UI instance.
+            if (actionUI == null)
+                actionUI = ActionUI.Instance;
+        }
+
+        // Animations
+        // Plays an animation.
+        public void PlayAnimation(string animationName)
+        {
+            // This doesn't check for an empty animation since this script...
+            // Only works when tied to animations.
+            animator.Play(animationName);
+        }
+
+        // Plays the empty state animation.
+        public void PlayEmptyStateAnimation()
+        {
+            PlayAnimation(emptyStateAnim);
+        }
+
+        // Plays the blink animation.
+        public void PlayBlinkingAnimation()
+        {
+            PlayAnimation(blinkAnim);
+        }
+
+        // Called when the blink animation begins.
+        public void OnBlinkAnimationStart()
+        {
+
+        }
+
+        // Called when the blink animation ends.
+        public void OnBlinkAnimationEnd()
+        {
+            PlayEmptyStateAnimation();
+        }
+
+        // Update is called once per frame
+        void Update()
+        {
+
+        }
+    }
+}
